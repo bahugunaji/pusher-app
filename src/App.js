8ffd70335a18996c58701454b450e276da376241
Gulshan Kumar <kumar.gulshan80@gmail.com>
2020-04-01 20:19:50 +0530

    did logical updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import Contacts from './Components/Contacts/contacts';
-import './App.css';
+import React, { Component } from "react";
+import Contacts from "./Components/Contacts/contacts";
+import "./App.css";
 
 class App extends Component {
-  
-  render() {
-        return (
-            <Contacts contacts={this.state.contacts} />
-        )
-    }
-
-    state = {
-        contacts: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      isError: false,
+      isLoading: false
     };
+  }
 
-    componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ contacts: data })
-            })
-            .catch(console.log)
-    }
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          contacts: data
+        });
+      })
+      .catch(err => {
+        this.setState({
+          isError: true
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isError && <div>Getting API error</div>}
+        <Contacts
+          isLoading={this.state.isLoading}
+          contacts={this.state.contacts}
+        />
+      </div>
+    );
+  }
 }
-
 
 export default App;
